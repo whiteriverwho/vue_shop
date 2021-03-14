81e35437f165286a2876f16461bc1e41dd534639
river <17792398846@163.com>
2021-03-14 22:29:21 +0800

    修改报错

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -39,38 +39,37 @@
   </div>
 </template>
 
-
 <script>
 export default {
   data() {
     return {
       // 数据绑定
       loginForm: {
-        username: "admin",
-        password: "123456",
+        username: 'admin',
+        password: '123456'
       },
       // 表单验证规则
       loginFormRules: {
         username: [
-          { required: true, message: "请输入登录名", trigger: "blur" },
+          { required: true, message: '请输入登录名', trigger: 'blur' },
           {
             min: 3,
             max: 10,
-            message: "登录名长度在3 到 10个字符",
-            trgger: "blur",
-          },
+            message: '登录名长度在3 到 10个字符',
+            trgger: 'blur'
+          }
         ],
         password: [
-          { required: true, message: "请输入密码", trigger: blur },
+          { required: true, message: '请输入密码', trigger: blur },
           {
             min: 6,
             max: 15,
-            message: "密码长度在6 到 15个字符",
-            trgger: "blur",
-          },
-        ],
-      },
-    };
+            message: '密码长度在6 到 15个字符',
+            trgger: 'blur'
+          }
+        ]
+      }
+    }
   },
   // 添加行为
   methods: {
@@ -78,33 +77,33 @@ export default {
     resetLoginForm() {
       // this = 当前组件对象, 其中的属性$refs 包含了设置的表单ref
       // console.log(this)
-      this.$refs.loginFormRef.resetFields();
+      this.$refs.loginFormRef.resetFields()
     },
     login() {
       // 点击登录的时候先调用validate方法验证表单内容是否有误
       this.$refs.loginFormRef.validate(async (valid) => {
-        console.log(this.loginFormRules);
+        console.log(this.loginFormRules)
         // 如果valid参数为true则验证通过
         if (!valid) {
-          return;
+          return
         }
 
         // 发送请求进行登录
-        const { data: res } = await this.$http.post("login", this.loginForm);
-        console.log(res);
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        console.log(res)
         if (res.meta.status !== 200) {
-          return this.$message.error("登录失败:" + res.meta.msg);
+          return this.$message.error('登录失败:' + res.meta.msg)
         }
-        this.$message.success("登录成功");
-        console.log(res);
+        this.$message.success('登录成功')
+        console.log(res)
         // 保存tonken
-        window.sessionStorage.setItem("token", res.data.token);
+        window.sessionStorage.setItem('token', res.data.token)
         // 导航至home
-        this.$router.push("/home");
-      });
-    },
-  },
-};
+        this.$router.push('/home')
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
