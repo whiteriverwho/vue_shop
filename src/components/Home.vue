796dff8893c2db7ab5936f496c8ddd9f053e7d26
river <17792398846@163.com>
2021-03-19 21:43:33 +0800

    菜单开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,122 @@
 <template>
-  <div>
-    this is home
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <!-- logo -->
+        <img src="../assets/logo.png" alt="" />
+        <!-- 顶部标题 -->
+        <span>电商管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="this.isCollapse ? '64px':'200px'">|||</div>
+        <!-- 侧边栏菜单 :collapse="isCollapse" (设置折叠菜单为绑定的 isCollapse 值)，
+        :collapse-transition="false" (关闭默认的折叠动画)-->
+        <el-menu
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router: default-active="activePath"
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu :index="'/' + subItem.path" v-for="item in menuList" :key="item.id" >
+            <!-- 一级菜单模板 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级子菜单 -->
+            <el-menu-item :index="subItem.id + ''" v-for="subItem in item.children"
+            :key="subItem.id" @click="saveNavState('/' + subItem.path)">
+              <!-- 二级菜单模板 -->
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>子菜单一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主体结构 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+
+  data() {
+    return {
+      // 左侧菜单数据
+      menuList: null,
+      iconsObj: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      activePath: null
+    }
+  },
+
+  created() {
+    // 在created阶段请求左侧菜单数据
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+
+    async getMenuList() {
+      // 发送请求获取左侧菜单数据
+      const { data: res } = await this.$http.get('menu')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+
+      this.menuList = res.data
+      console.log(res)
+    },
+
+    saveNavState(path) {
+      // 点击二级菜单的时候保存被点击的二级菜单信息
+      window.sessionStorage.setItem('activePath', path)
+      this.activePath = path
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-el-header {
+  background-color: #373d41;
+}
+.el-el-aside {
+  background-color: #333744;
+}
+.el-el-main {
+  background-color: #eaedf1;
+}
+
 </style>
