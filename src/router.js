c78665dc57ddc2f90ead447637bb2c960feee60f
river <17792398846@163.com>
2021-04-04 11:54:03 +0800

    添加权限列表组件

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Welcome from './/components/Welcome.vue'
 import Users from '@/components/user/Users'
+import Rights from '@/components/power/Rights'
+import Roles from '@/components/power/Roles'
 
 Vue.use(Router)
 
@@ -19,7 +21,9 @@ const router = new Router({
         { path: '/welcome', component: Welcome }
       ]
     },
-    { path: '/users', component: Users }
+    { path: '/users', component: Users },
+    { path: '/rights', component: Rights },
+    { path: '/roles', component: Roles }
   ]
 })
 
